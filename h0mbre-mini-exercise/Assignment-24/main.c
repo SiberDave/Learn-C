abafb2700146c44db9007e628a039fda09c58c11
SiberDave <dave.cloud.cyber@gmail.com>
2024-07-21 20:32:01 +0000

    Assignment 24 is completed

---
+++ h0mbre-mini-exercise/Assignment-24/main.c
@@ -0,0 +1,24 @@
+// For this excercise we're going to build a C program that executes syscalls! 
+// We've already learned basically everything we need to know in C up until this point to 
+// accomplish this. We are going to keep it simple and we're going to use the open() 
+// syscall only here. It should be noted that technically we are not making syscalls directly in user 
+// programs, but instead are using wrapper functions defined in libc for our purposes.
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(){
+    int openfile;
+    
+    openfile = open("testfile.txt", O_WRONLY | O_CREAT, S_IRWXU);
+
+    if (openfile < 0){
+        printf("Open operation failed!\n");
+        return -1;
+    }
+    else {
+        printf("Open operation succeeded!\n");
+        return 0;
+    }
+}
