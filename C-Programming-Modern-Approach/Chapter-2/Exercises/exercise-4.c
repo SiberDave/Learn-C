8984e1830352d47ce901116477703a46b3fb3b38
SiberDave <dave.cloud.cyber@gmail.com>
2024-08-27 19:37:33 +0000

    Work on Exercise chapter 2 number 4 - 6

---
+++ C-Programming-Modern-Approach/Chapter-2/Exercises/exercise-4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int a, b;
+    float c, d;
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%f\n", c);
+    printf("%f\n", d);
+}
---
+++ C-Programming-Modern-Approach/Chapter-2/Exercises/exercise-6.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    int current___________balance = 100;
+
+    printf("%d\n", current___________balance);
+}
