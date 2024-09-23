6eecde1709dfb953fe150b54c5ec446572474c03
Dave Malvin <dm.enterprienur@gmail.com>
2024-09-24 03:07:02 +0700

    Update on beej book for chapter 2 and 3 of example code

---
+++ Beej’s_Guide_to_C_Programming/Chapter3/hello_var.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i; // holds signed interger such as -3, -2, 0, 1, 10
+    float j; // holds signed floating point number such as -3.1415
+
+    printf("Hello, World!\n");
+}
---
+++ Beej’s_Guide_to_C_Programming/Chapter3/hello_var_store.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i; 
+    i = 2; // Assign value 2 to variable i 
+
+    printf("Hello, World!\n");
+}
