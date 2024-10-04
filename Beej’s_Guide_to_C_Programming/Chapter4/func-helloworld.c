4f9136cbc6269742aa88ea8e395c880984e19ab3
Dave Malvin <dm.enterprienur@gmail.com>
2024-10-04 12:13:14 +0700

    Start Chapter 4, function hello world and pass by value

---
+++ Beej’s_Guide_to_C_Programming/Chapter4/func-helloworld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void hello(void){
+    printf("Hello World!\n");
+}
+
+int main(void){
+    hello();
+}
---
+++ Beej’s_Guide_to_C_Programming/Chapter4/passbyvalue.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void increment(int a){
+    a++;
+}
+
+int main(void){
+    int i = 10;
+
+    increment(i);
+
+    printf("i == %d\n", i);
+}
